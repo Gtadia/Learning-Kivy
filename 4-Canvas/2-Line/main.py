1fd93e0cf778e6eef3106f7225240acd56ca80b8
Gtadia <m235humit9@gmail.com>
2022-07-20 15:52:06 -0400

    Finished 1-Rectangle_and_Ellipse

---
+++ 4-Canvas/2-Line/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class CanvasExample1(Widget):
+    pass
+
+    
+   
+
+class FirstKivyApp(App):
+    pass
+
+if __name__ == "__main__":
+    FirstKivyApp().run()
