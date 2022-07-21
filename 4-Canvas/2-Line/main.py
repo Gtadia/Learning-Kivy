a75fab10a3c4e18ec3d9fa21cad5bc72d113b69e
Gtadia <m235humit9@gmail.com>
2022-07-21 16:15:21 -0400

    Finished exercise

--- 4-Canvas/2-Line/main.py
+++ 4-Canvas/2-Line/main.py
@@ -5,6 +5,10 @@ from kivy.uix.widget import Widget
 class CanvasExample1(Widget):
     pass
 
+
+# Kivy Exercise (Making a 2 by 2 gird that is dependent on the window size)
+class CanvasExample2(Widget): 
+    pass
     
    
 
