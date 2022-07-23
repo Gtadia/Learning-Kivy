33f8027efcd1c651c2efb7e892215e0e5885f984
Gtadia <m235humit9@gmail.com>
2022-07-23 13:29:44 -0400

    FRENCH INTENSIFIES

---
+++ 4-Canvas/5-Coordinates_and_RelativeLayout/main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Ellipse, Line, Rectangle
+from kivy.metrics import dp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+from kivy.properties import Clock 
+
+
+class CanvasExample6(Widget):
+    pass
+
+
+"""EXERCISE"""
+class CanvasExample6Exercise(BoxLayout):
+    pass
+"""EXERCISE"""
+"""
+Make the French Flag
+"""
+
+
+class FirstKivyApp(App):
+    pass
+
+if __name__ == "__main__":
+    FirstKivyApp().run()
